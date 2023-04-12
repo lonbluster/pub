aba6d02b4dcb4870cc48c7c214d404f726fe6f41
gaspa <lonbluster@gmail.com>
2023-04-12 23:00:44 +0300

    uptime

--- cmdEvents.ps1
+++ cmdEvents.ps1
@@ -10,7 +10,7 @@ $errorEvents = $(Get-WinEvent -ErrorAction Continue -WarningAction Continue -Max
 $model = (Get-CimInstance -ClassName Win32_ComputerSystem).model
 $diskspace= (Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'Size (GB)'; Expression= {[int]($_.size / 1GB) }},@{'Name' = 'Free (GB)'; Expression= { [int]($_.Freespace / 1GB) }})
 $uptime= ([math]::Round(((get-date) - (gcim Win32_OperatingSystem).LastBootUpTime).totaldays/1,2))
-$body = @{Body = "$model `n Giorni Uptime :$uptime `n $(Out-String -InputObject $diskspace) `n  Attached last $hours hours of logs... `n `n-------------------  Last 5 Errors Only :  `n  
+$body = @{Body = " Model: $model `n Giorni Uptime :$uptime `n $(Out-String -InputObject $diskspace) `n  Attached last $hours hours of logs... `n `n-------------------  Last 5 Errors Only :  `n  
     $(Out-String -InputObject $errorEvents) "}
 $mailParams = @{
         SmtpServer                 = 'smtps.aruba.it'
